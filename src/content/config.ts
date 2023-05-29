1f581a07969fc05604d7fb9782c0d98965cc47fc
차하늘 <haneul.cha@nrise.net>
2023-05-29 16:32:58 +0900

    feat: review

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -14,8 +14,30 @@ const blog = defineCollection({
       .string()
       .optional()
       .transform((str) => (str ? new Date(str) : undefined)),
+    tags: z.array(z.string()).optional(),
     heroImage: z.string().optional(),
+    relatedPosts: z.array(reference("blog")).optional(),
+    isDraft: z.boolean().optional(),
   }),
 });
 
-export const collections = { blog };
+const bobsBurgers = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    season: z.number(),
+    episodeNumber: z.number(),
+    rate: z.number(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+  }),
+});
+
+export const collections = { blog, bobsBurgers };
